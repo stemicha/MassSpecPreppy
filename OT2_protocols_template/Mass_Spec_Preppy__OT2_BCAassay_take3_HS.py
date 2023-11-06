cab2eb2b4444ebec95839b763ff9e5f851202a2b
stemicha <stephan.michalik@uni-greifswald.de>
2023-11-06 16:15:57 +0100

    adjust initial take3 protocol for heat shaker usage

--- OT2_protocols_template/Mass_Spec_Preppy__OT2_BCAassay_take3_HS.py
+++ OT2_protocols_template/Mass_Spec_Preppy__OT2_BCAassay_take3_HS.py
@@ -21,7 +21,7 @@ def get_values(*names):
 #specify meta data
 metadata = {
      "protocolName": "BCA protein concentration determination with OT-2 with CSV File",
-     "apiLevel": "2.12",
+     "apiLevel": "2.13",
      "description": "BCA assay for take3 plate",
      "author": "Stephan Michalik <stephan.michalik@uni-greifswald.de>"}
 
@@ -60,7 +60,8 @@ def run(protocol: protocol_api.ProtocolContext):
                             "A6","B6","C6","D6","E6","F6","G6","H6"]
   
   sample_dilution_end_volume = 50
-  standard_initial_volume = 350
+  standard_dilution_end_volume = 50
+  standard_initial_volume = 400
   sample_number = len(SampleTransfer)
   #min. sample dilutuion end-volume [µl]
   min_vol_sample_dilution = 25
@@ -84,28 +85,32 @@ def run(protocol: protocol_api.ProtocolContext):
         SampleTransfer[i].append(min_sample_volume_transfer*float(SampleTransfer[i][1]))
 
   
+  #modules
+  hs_mod = protocol.load_module("heaterShakerModuleV1", "7")
+
   #tips
-  tiprack20_1 = protocol.load_labware("opentrons_96_tiprack_20ul", "7",label = "opentrons_96_tiprack_20ul_1")
-  tiprack20_2 = protocol.load_labware("opentrons_96_tiprack_20ul", "8",label = "opentrons_96_tiprack_20ul_2")
-  tiprack20_3 = protocol.load_labware("opentrons_96_tiprack_20ul", "9",label = "opentrons_96_tiprack_20ul_3")
-  tiprack20_4 = protocol.load_labware("opentrons_96_tiprack_20ul", "6",label = "opentrons_96_tiprack_20ul_4")
+  tiprack20_1 = protocol.load_labware("opentrons_96_tiprack_20ul", "8",label = "opentrons_96_tiprack_20ul_1")
+  tiprack20_2 = protocol.load_labware("opentrons_96_tiprack_20ul", "4",label = "opentrons_96_tiprack_20ul_2")
+  tiprack20_3 = protocol.load_labware("opentrons_96_tiprack_20ul", "5",label = "opentrons_96_tiprack_20ul_3")
+  tiprack20_4 = protocol.load_labware("opentrons_96_tiprack_20ul", "9",label = "opentrons_96_tiprack_20ul_4")
 
   #plates
-  reagent_plate = protocol.load_labware("nest_12_reservoir_15ml", "3",label = "reagent_plate_nest_12_reservoir_15ml")
+  reagent_plate = protocol.load_labware("nest_12_reservoir_15ml", "2",label = "reagent_plate_nest_12_reservoir_15ml")
   #A1 = water min. 5ml
   #B1 = 1x sample buffer min. 10ml
   #C1 = 2x sample buffer min. 5ml
   #D1 = BCA working reagent min . 10ml
-  prep_plate = protocol.load_labware("nest_96_wellplate_100ul_pcr_full_skirt", "2", label = "preparation_plate")
-  BCA_plate = protocol.load_labware("nest_96_wellplate_100ul_pcr_full_skirt", "1", label  = "BCA_plate__half_area")
+  BCA_plate = protocol.load_labware("nest_96_wellplate_100ul_pcr_full_skirt", "1", label = "BCA_PCR_plate")
+  prep_plate = hs_mod.load_labware("opentrons_96_pcr_adapter_nest_wellplate_100ul_pcr_full_skirt", label  = "preparation_PCR_plate")
+
   #samples
-  samples1 = protocol.load_labware("opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap", "4",label = "samples1")
-  samples2 = protocol.load_labware("opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap", "5",label = "samples2")
+  samples1 = protocol.load_labware("opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap", "6",label = "samples1")
+  samples2 = protocol.load_labware("opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap", "3",label = "samples2")
   # D6 samples 2 = BSA stock 1mg/ml min. 350µl
   
   #pipettes
-  p20 = protocol.load_instrument("p20_single_gen2", mount = "left", tip_racks = [tiprack20_1,tiprack20_2,tiprack20_3])
-  m20 = protocol.load_instrument("p20_multi_gen2", mount = "right", tip_racks=[tiprack20_4])
+  p20 = protocol.load_instrument("p20_single_gen2", mount = "left", tip_racks = [tiprack20_1,tiprack20_2])
+  m20 = protocol.load_instrument("p20_multi_gen2", mount = "right", tip_racks=[tiprack20_3,tiprack20_4])
 
   #wells for transfer to plates with multichannel pipette
   prep_columns_positions = ["A2","A3","A4","A5","A6"]
@@ -114,13 +119,21 @@ def run(protocol: protocol_api.ProtocolContext):
 
   #generate standard position and volumes
   std_positions = ["A1","B1","C1","D1","E1","F1","G1"]
-  std_volumes = [24,18,12,9,6,3,1.5]
+  std_volumes = [16,12,8,6,4,2,1]
 
   # offset piptette over bottom: default 1mm
   bottom_offset = 1.5
 
+  # close latch HS
+  hs_mod.close_labware_latch()
+
   #std. dilution: transfer 2xBSA diluent
-  m20.transfer(75, reagent_plate.columns_by_name()["1"], prep_plate.columns_by_name()["1"])
+  m20.transfer(standard_dilution_end_volume,
+              reagent_plate.wells_by_name()["A1"],
+              prep_plate.wells_by_name()["A1"],
+              touch_tip = True,
+              new_tip = "always",
+              disposal_volume = 0)
       
 
   #refine 2xBSA diluent with P20 / use one tip
@@ -140,7 +153,8 @@ def run(protocol: protocol_api.ProtocolContext):
                 new_tip ="always", 
                 touch_tip = True,
                 mix_after = (2, 20), 
-                blowout_location = 'destination well')
+                disposal_volume = 0)
+                
     #standard height
     standard_initial_volume = standard_initial_volume - std_volumes[i]
 
@@ -169,7 +183,6 @@ def run(protocol: protocol_api.ProtocolContext):
                                                     new_tip ="always", 
                                                     touch_tip = True,
                                                     mix_after = (2, 20), 
-                                                    blowout_location = 'destination well',
                                                     disposal_volume = 0)
                 
        
@@ -179,13 +192,16 @@ def run(protocol: protocol_api.ProtocolContext):
                       new_tip ="always", 
                       touch_tip = True,
                       mix_after = (2, 20), 
-                      blowout_location = 'destination well',
                       disposal_volume = 0)
     
     #sample height adjustment
     for k in range(0,len(SampleTransfer)):
       if SampleTransfer[i][0]==SampleTransfer[k][0]: SampleTransfer[k][4] = float(SampleTransfer[k][4])-SampleTransfer[i][6]
-    
+  
+  #shake plate
+  hs_mod.set_and_wait_for_shake_speed(2000)
+  protocol.delay(minutes=2)
+  hs_mod.deactivate_shaker() 
 
   #distribute working solution to BCA plate
   m20.pick_up_tip()
@@ -211,7 +227,7 @@ def run(protocol: protocol_api.ProtocolContext):
                   [BCA_plate.wells_by_name()[well_name].bottom(bottom_offset) for well_name in ["A1","A2"]],
                   new_tip = "once",
                   touch_tip = False,
-                  mix_before = [8,20],
+                  mix_before = [2,20],
                   disposal_volume = 0)
   
   #distribute sample to BCA plate
@@ -220,7 +236,7 @@ def run(protocol: protocol_api.ProtocolContext):
                                                            [BCA_plate.wells_by_name()[well_name].bottom(bottom_offset) for well_name in BCA_columns_position[i]],
                                                            new_tip = "once",
                                                            touch_tip = False,
-                                                           mix_before = [8,20],
+                                                           mix_before = [2,20],
                                                            disposal_volume = 0)
  
 
